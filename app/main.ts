a478263be5c164bd61e51ce488752baf83d04c04
Pranjal <spranjal394@gmail.com>
2024-09-08 10:50:52 +0530

    stage-1-UDP_Sever

--- app/main.ts
+++ app/main.ts
@@ -5,15 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 //
-// const udpSocket: dgram.Socket = dgram.createSocket("udp4");
-// udpSocket.bind(2053, "127.0.0.1");
-//
-// udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
-//     try {
-//         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-//         const response = Buffer.from("");
-//         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
-//     } catch (e) {
-//         console.log(`Error sending data: ${e}`);
-//     }
-// });
+const udpSocket: dgram.Socket = dgram.createSocket("udp4");
+udpSocket.bind(2053, "127.0.0.1");
+
+udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+    try {
+        console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
+        const response = Buffer.from("");
+        udpSocket.send(response, remoteAddr.port, remoteAddr.address);
+    } catch (e) {
+        console.log(`Error sending data: ${e}`);
+    }
+});
